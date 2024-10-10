499d0f6f19552b723c920a934e7d9218ec0cd583
Daniel Bolding <ratnoteb@gmail.com>
2024-10-10 19:18:10 +0000

    Event and location models.

--- stars_app/models.py
+++ stars_app/models.py
@@ -1,4 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=30)
+    type = models.CharField(max_length=30)
+    viewing_radius = models.IntegerField(default=10)
+    peak_time = models.DateTimeField(default=timezone.now())
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=30)
+    zip_code = models.IntegerField(default=00000)
+    latitude = models.FloatField()
+    latitude_direction = models.CharField(max_length=1)
+    longitude = models.FloatField()
+    longitude_direction = models.CharField(max_length=1)
