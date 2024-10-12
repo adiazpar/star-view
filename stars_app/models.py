406e12e5a8e429bf1caefed460b3311902f3ab06
adiazpar <alexdiaz0923@gmail.com>
2024-10-12 14:37:12 -0600

    Navbar overhaul and slight changes to home screen. Home screen is subject to change...

--- stars_app/models.py
+++ stars_app/models.py
@@ -3,13 +3,6 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 # Create your models here.
-class Event(models.Model):
-    name = models.CharField(max_length=30)
-    event_type = models.CharField(max_length=30)
-    viewing_radius = models.IntegerField(default=10)
-    peak_time = models.DateTimeField(default=timezone.now())
-    location = models.ForeignKey(Location, on_delete=models.CASCADE)
-
 class Location(models.Model):
     location_name = models.CharField(max_length=30)
     zip_code = models.IntegerField(default=00000)
@@ -17,3 +10,10 @@ class Location(models.Model):
     latitude_direction = models.CharField(max_length=1)
     longitude = models.FloatField()
     longitude_direction = models.CharField(max_length=1)
+
+class Event(models.Model):
+    name = models.CharField(max_length=30)
+    event_type = models.CharField(max_length=30)
+    viewing_radius = models.IntegerField(default=10)
+    peak_time = models.DateTimeField(default=timezone.now())
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
